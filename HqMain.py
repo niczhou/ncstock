0a66c92dd19156ed0640b47ae102dfe1aeb91e59
niczhou <madeozhou@sina.com>
2017-12-26 17:04:48 +0800

    updatelist

--- HqMain.py
+++ HqMain.py
@@ -6,23 +6,38 @@ import datetime
 from HqAnalyst import HqAnalyst
 from HqUtil import HqUtil
 from HsUpdater import HsUpdater
+from HsWorm import HsWorm
 
 conn = pymysql.connect(host="localhost",user="root",passwd="",db="stock",charset="utf8")
 
 mAnalyst = HqAnalyst(conn)
 
-# for i in range(10,100):
+# bUrl="http://www.fjbs.gov.cn/eWebEditor/uploadfile/20171103100531556.xlsx"
+# mWorm=HsWorm()
+# 
+# mWorm.downloadf(bUrl)
+# mWorm.readUrl('http://www.fjbs.gov.cn/eWebEditor/uploadfile/20171103100531556.xlsx')
+
+sql="SELECT stock_code FROM listsz"
+cursor=conn.cursor()
+cursor.execute(sql)
+stockArr=cursor.fetchall()
+print(len(stockArr))
+
+# for i in range(0,len(stockArr)):
 #   try:
-#     isBuy=mAnalyst.getIsBuyByCode("6000"+str(i),"20170922",17)
-#     print("6000"+str(i)+":"+isBuy)
+#     isBuy=mAnalyst.getIsBuyByCode(str(stockArr[i][0]),"20170922",17)
+#     print(str(stockArr[i][0])+":"+isBuy)
 #   except:
 #     continue
 # 
-mUtil=HqUtil()
-print(mUtil.getStartDate("20171127",7,conn))
-
+# mUtil=HqUtil()
+# print(mUtil.getStartDate("20171127",7,conn))
+# 
 mHs=HsUpdater(conn)
 mHs.updateListsz('C:/demo/nzstock/input/20171203深圳A股列表.xlsx')
+mHs.updateListsh('C:/demo/nzstock/input/20171203上海A股列表.xlsx')
+
 # a=time.time()
 # print(a)
 # b=time.strptime(a,"%Y%m%d")
--- HsUpdater.py
+++ HsUpdater.py
@@ -16,19 +16,42 @@ class HsUpdater:
     def updateListsz(self,xlPath):       
         mBook=xlrd.open_workbook(xlPath)    
         mSheet=mBook.sheets()[0]
-        rowCount=mSheet.nrows
-
-        for i in range(1,rowCount+1):
-#             sql="INSERT INTO listsz(stock_code,stock_name,stock_ipo,stock_total,stock_circulation) VALUES(" \
-#             +mSheet.cell(i,5).value +","+mSheet.cell(i,6).value+","+mSheet.cell(i,7).value \
-#             +","+mSheet.cell(i,8).value +","+mSheet.cell(i,9).value+")"
-            sql="SELECT stock_code FROM listsz WHERE stock_code='"+mSheet.cell(i,5).value+"'"
-            self.__cursor.execute(sql)
-            result=self.__cursor.fetchone()
-            
-            if result==None:
-                sql="INSERT INTO listsz (stock_code) VALUES ('" \
-                +mSheet.cell(i,5).value+"')"
-                print(str(i)+":"+sql)
+        
+        for cel in mSheet.col_values(5):
+            if cel!="A股代码":        
+                sql="SELECT stock_code FROM listsz WHERE stock_code ='"+str(cel)+"'"
                 self.__cursor.execute(sql)
+                result=self.__cursor.fetchone()            
+                if result==None:
+                    sql="INSERT INTO listsz (stock_code) VALUES ('" +str(cel)+"')"
+                    print(cel+":"+sql)
+                    self.__cursor.execute(sql)
     
+    def updateListsh(self,xlpath):
+        mBook=xlrd.open_workbook(xlpath)
+        mSheet=mBook.sheets()[0]
+        
+        for cel in mSheet.col_values(2):
+            if cel!="A股代码":
+                sql="SELECT stock_code FROM listsh WHERE stock_code ='"+str(cel)+"'"
+                self.__cursor.execute(sql)
+                result=self.__cursor.fetchone()
+                if result==None:
+                    sql="INSERT INTO listsz(stock_code) VALUES('"+str(cel)+"')"
+                    print(cel+":"+sql)
+                    self.__cursor.execute(sql)
+                
+    def createList(self,shsz):
+        try:
+            sql="CREATE TABBLE list"+shsz+"(ID INT NOT NULL AUTO_INCREMENT, \
+            stock_code VARCHAR(255),stock_name VARCHAR(255),stock_ipo VARCHAR(255), \
+            stock_total VARCHAR(255),stock_circulation VARCHAR(255),PRIMARY KEY(ID))" 
+            self.__cursor.execute(sql)            
+        except:
+            print("can not CREATE table list"+shsz)
+            
+        
+        
+        
+        
+        
