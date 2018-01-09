ee8d83a850a220e36f73c166e24f92ebb56b8e18
niczhou <madeozhou@sina.com>
2018-01-09 17:05:49 +0800

    on duplicateupdate

--- HsUpdater.py
+++ HsUpdater.py
@@ -12,46 +12,60 @@ class HsUpdater:
     def __init__(self,connection):
         self.__conn=connection
         self.__cursor=connection.cursor()
-        
-    def updateListsz(self,xlPath):       
-        mBook=xlrd.open_workbook(xlPath)    
-        mSheet=mBook.sheets()[0]
-        
-        for cel in mSheet.col_values(5):
-            if cel!="A股代码":        
-                sq="SELECT stock_code FROM tablesz WHERE stock_code ='"+str(cel)+"'"
-                self.__cursor.execute(sq)
-                result=self.__cursor.fetchone()            
-                if result==None:
-                    sq="INSERT INTO tablesz (stock_code) VALUES ('" +str(cel)+"')"
-#                     print(cel+":"+sql)
-                    self.__cursor.execute(sq)
-    
-    def updateListsh(self,xlpath):
-        print("updatesh")
-        mBook=xlrd.open_workbook(xlpath)
-        mSheet=mBook.sheets()[0]
-        
-        for cel in mSheet.col_values(2):
-            if cel!="A股代码":
-                sql="SELECT stock_code FROM tablesh WHERE stock_code ='"+str(int(cel))+"'"
-                self.__cursor.execute(sql)
-                result=self.__cursor.fetchone()
-                if not result:
-                    sql="INSERT INTO tablesh(stock_code) VALUES('"+str(int(cel))+"')"
-#                     print(cel+":"+sql)
-                    self.__cursor.execute(sql)
-                
-    def createTableHs(self,shsz):
+
+    def createTableHs(self,hsTableName):
         try:
-            sq="CREATE TABLE IF NOT EXISTS table"+shsz+"(id INT NOT NULL AUTO_INCREMENT,"\
-            +"stock_code VARCHAR(4),stock_name VARCHAR(16),stock_ipo INT,"\
+            sq="CREATE TABLE IF NOT EXISTS "+hsTableName+"(id INT NOT NULL AUTO_INCREMENT,"\
+            +"stock_code VARCHAR(8),stock_name VARCHAR(16),stock_ipo INT,"\
             +"stock_total BIGINT,stock_circulation BIGINT,PRIMARY KEY(id))" 
             print(sq)
             self.__cursor.execute(sq)            
         except:
             print("can not CREATE table list"+shsz)
+        
+    def updateTableHs(self,shsz,xlPath):       
+        mBook=xlrd.open_workbook(xlPath)    
+        mSheet=mBook.sheets()[0]
+        
+        if shsz=="tablesz":
+            listCode=mSheet.col_values(5)
+        else:
+            listCode=mSheet.col_values(2)
             
+        for cel in listCode:
+            if cel!="A股代码":        
+                sq="INSERT INTO "+shsz+"(stock_code) VALUES ('" +str(cel)+"')"\
+                    +"WHERE NOT EXISTS(SELECT stock_code FROM "+shsz+" WHERE stock_code="+cel+")"
+                
+#                     print(cel+":"+sql)
+                self.__cursor.execute(sq)
+    
+#     def updateTableSh(self,xlpath):
+#         print("updatesh")
+#         mBook=xlrd.open_workbook(xlpath)
+#         mSheet=mBook.sheets()[0]
+#         
+#         for cel in mSheet.col_values(2):
+#             if cel!="A股代码":
+#                 sql="SELECT stock_code FROM tablesh WHERE stock_code ='"+str(int(cel))+"'"
+#                 self.__cursor.execute(sql)
+#                 result=self.__cursor.fetchone()
+#                 if not result:
+#                     sql="INSERT INTO tablesh(stock_code) VALUES('"+str(int(cel))+"')"
+# #                     print(cel+":"+sql)
+#                     self.__cursor.execute(sql)
+######################################################################################
+    def createCodeTable(self,stockCode):
+        try:
+            sq="CREATE TABLE IF NOT EXISTS `"+stockCode+"`(id INT NOT NULL AUTO_INCREMENT,"\
+                +"trade_date int,`open` FLOAT,`close` FLOAT,`change`"\
+                +" FLOAT,`percent` FLOAT,`low` FLOAT,`high` FLOAT,"\
+                +"volume DOUBLE,amount DOUBLE,turnover FLOAT,PRIMARY KEY(ID));"
+            print(sq)
+            self.__cursor.execute(sq)    
+        except:
+            print("can not CREATE table "+stockCode)
+             
     def updateCodeTables(self):
         sq="SELECT stock_code FROM tablesh"
         self.__cursor.execute(sq)
@@ -67,15 +81,6 @@ class HsUpdater:
         for j in range(len(listSz)):
             self.createCodeTable(listSz[j])
         
-    def createCodeTable(self,stockCode):
-        try:
-            sq="CREATE TABLE IF NOT EXISTS `"+stockCode+"`(id INT NOT NULL AUTO_INCREMENT,"\
-                +"trade_date int,`open` FLOAT,`close` FLOAT,`change`"\
-                +" FLOAT,`percent` FLOAT,`low` FLOAT,`high` FLOAT,"\
-                +"volume DOUBLE,amount DOUBLE,turnover FLOAT,PRIMARY KEY(ID));"
-            print(sq)
-            self.__cursor.execute(sq)    
-        except:
-            print("can not CREATE table "+stockCode)
+
         
         
